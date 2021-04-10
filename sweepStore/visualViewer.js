d1dea2a9c87946c9fd27efd882fd58f546eb5683
Ernesto <56931380+Ernest0G@users.noreply.github.com>
2021-04-09 17:15:31 -0700

    add: a working HTML table display

--- sweepStore/visualViewer.js
+++ sweepStore/visualViewer.js
@@ -1,54 +1,53 @@
-const { dataOrg, parse } = require("./dataOrganizer");
-dataOrg();
-
 function readFile()
 {
-  parse();
-
+  $.ajax({
+    url: 'dummyData/cereal.csv',
+    dataType: 'text',
+  }).done(showInitialFormat,showSweepFormat);
 }
 
 
-function showInitialFormat(data) 
-{
-    var tableDisplay = document.getElementsByTagName("TableDisplay")[0];
-    var allRows = data.split(/\r?\n|\r/);
-    var table = document.createElement("table");
-
-    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+function showInitialFormat(data) {
+  var allRows = data.split(/\r?\n|\r/);
+  var table = '<table>';
+  
+  for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+    if (singleRow === 0) {
+      table += '<thead>';
+      table += '<tr>';
+    } else {
+      table += '<tr>';
+    }
+    var rowCells = allRows[singleRow].split(',');
+    for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
       if (singleRow === 0) {
-        table += '<thead>';
-        table += '<tr>';
+        table += '<th>';
+        table += rowCells[rowCell];
+        table += '</th>';
       } else {
-        table += '<tr>';
+        table += '<td>';
+        table += rowCells[rowCell];
+        table += '</td>';
       }
-      var rowCells = allRows[singleRow].split(',');
-      for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
-        if (singleRow === 0) {
-          table += '<th>';
-          table += rowCells[rowCell];
-          table += '</th>';
-        } else {
-          table += '<td>';
-          table += rowCells[rowCell];
-          table += '</td>';
-        }
-      }
-      if (singleRow === 0) {
-        table += '</tr>';
-        table += '</thead>';
-        table += '<tbody>';
-      } else {
-        table += '</tr>';
-      }
-    } 
-    table += '</tbody>';
-    table += '</table>';
-    $('TableDisplay').append(table);
+    }
+    if (singleRow === 0) {
+      table += '</tr>';
+      table += '</thead>';
+      table += '<tbody>';
+    } else {
+      table += '</tr>';
+    }
+  } 
+  table += '</tbody>';
+  table += '</table>';
+  $('.columnL').append(table);
 }
 
-function showSweepFormat()
+function showSweepFormat(data)
 {
+  var tree = '<ul>'
 
+  $('.columnR').append(tree);
 }
 
 function editFormat()
