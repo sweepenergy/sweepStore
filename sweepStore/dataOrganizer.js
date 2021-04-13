ceae0fabb3dbec714ee91ec6e0c25c8604de9dad
zephr4 <75108866+zephr4@users.noreply.github.com>
2021-04-12 19:06:52 -0700

    Change: revert to last push

--- sweepStore/dataOrganizer.js
+++ sweepStore/dataOrganizer.js
@@ -16,19 +16,66 @@
 
 
 const path = require('path');
-const csvPath = path.resolve(__dirname, './dummyData/test.csv')
+const csvPath = path.resolve(__dirname, './dummyData/cereal.csv')
 
 const fs = require('fs');
 const Papa = require('papaparse');
 const fastcsv = require('fast-csv');
-const writer = fs.createWriteStream("./dummydata/data.csv");
+//const writer = fs.createWriteStream("./dummydata/data.csv");
 
-/*
-As information is being streamed through the server connection we can perform checks on the key values of the JSON object
-to determine how the data will be posted
-*/
+async function parse(csvFilePath) {
+    //const csvFile = fs.readFileSync(csvFilePath)
+    //const csvData = csvFile.toString() 
+    const fileStream = fs.createReadStream(csvFilePath, {highWaterMark: 1000}); 
+     
+    Papa.parse(fileStream, {
+        header: true,
+        dynamicTyping: true,
+        //chunkSize: fileStream.readableHighWaterMark/100,
+        //chunkSize: 1000,
+        chunk: function(results, parse) {
+            console.log("Chunk data:", results.data);
+            //console.log("chunkSize:", this.chunkSize); 
+            //console.log(fileStream.readableHighWaterMark); 
+            console.log("--------------Chunk end---------------"); 
+        },
+        complete: results => {
+            //console.log('Complete', results.data.length, 'records.'); 
+
+            //let columnNames = results.meta.fields; 
+
+            ////Array of JSONs 
+            //let dataCSV = results.data; 
+            ////resolve(dataCSV);
+    
+            ////Size of dataCSV (Number of jsons/elements the array has has)
+            //let totSize = dataCSV.length;
+            //
+            ////Size of an individual json 
+            //let jsonSize = Object.keys(dataCSV[0]).length;
+            //
+            //var val = []; 
+            //let tempJson;
+            //for(let i = 0; i < columnNames.length; i++) {
+            //    let colValues = [];
+            //    colValues.push(columnNames[i]);
+            //    for(let j = 0; j < totSize; j++) {
+            //        tempJson = dataCSV[j];
+            //        for (const [key, value] of Object.entries(tempJson)) {
+            //            if(key == columnNames[i]) {
+            //                colValues.push(value);
+            //            }
+            //        }
+            //    }
+            //    val.push(colValues);
+            //}
+            //resolve(val);
+        }
+    }); 
+}
+
 function test(csvFilePath) {
-    const csvData = fs.createReadStream(csvFilePath, {highWaterMark: 16});
+    const csvData = fs.createReadStream(csvFilePath, {highWaterMark: 1000});
 
     var data=[];
     Papa.parse(csvData, {
@@ -37,6 +84,7 @@ function test(csvFilePath) {
       step: function(result) {
         data.push(result.data)
         console.log("result: ", result.data);
+        console.log("---------Chunk end-----------"); 
       },
       complete: function(results, file) {
         console.log('Complete', data.length, 'records.'); 
@@ -44,67 +92,23 @@ function test(csvFilePath) {
     });
 }
 
-async function parse(csvFilePath) {
-    const csvFile = fs.readFileSync(csvFilePath) //check if this lets you read in chunks
-    const csvData = csvFile.toString()  
-    return new Promise(resolve => {
-        Papa.parse(csvData, {
-        //check to import in chunks
-        header: true,
-        dynamicTyping: true,
-        complete: results => {
-            console.log('Complete', results.data.length, 'records.'); 
 
-            let columnNames = results.meta.fields; 
-            
-            //Array of JSONs 
-            let dataCSV = results.data; 
-            resolve(dataCSV);
-            //Size of dataCSV (Number of jsons/elements the array has has)
-            let totSize = dataCSV.length;
-            
-            //Size of an individual json 
-            let jsonSize = Object.keys(dataCSV[0]).length;
-            
-            // Here we can break the JSON object into an array
-            var val = []; 
-            let tempJson;
-            for(let i = 0; i < columnNames.length; i++) {
-                let colValues = [];
-                colValues.push(columnNames[i]);
-                for(let j = 0; j < totSize; j++) {
-                    tempJson = dataCSV[j];
-                    for (const [key, value] of Object.entries(tempJson)) {
-                        if(key == columnNames[i]) {
-                            colValues.push(value);
-                        }
-                    }
-                }
-                val.push(colValues);
-            }
-            //resolve(val);
-        }
-        });
-    });
-}
-
-function dataOrg() {
-    //We currently have the tranpose of the dataset
-    var data = test(csvPath);
+async function dataOrg() {
+    //Esstenially we currently have the tranpose of the dataset
+    var data = await parse(csvPath);
     //console.log(data);
-
     // data.foreach(element){
     //     dict[element] = json_decode(json_encode($a));
     // }
-
-    // Lets us write to a file defined by ws
-    // fastcsv
-    //     .write(data, { headers: true })
-    //     .pipe(writer);
-    // console.log(data);
+    //Lets us write to a file defined by ws
+    //fastcsv
+    //.write(data, { headers: true })
+    //.pipe(writer);
+    //console.log(data);
 }
 
 dataOrg();
+//test(csvPath); 
 
 //allows us to export the function as a module to be used by other files
 module.exports = {dataOrg, parse};
