4204fb0d5bda141d12b81b99ad85706c30df89f9
zephr4 <andyalvarenga4@gmail.com>
2021-04-17 23:15:05 -0700

    Add: Created a redirect schema between the login and the home page

--- src/server.js
+++ src/server.js
@@ -1,43 +1,24 @@
-/*const express = require('express');
-const app = express();
-const router = express.Router();
-const fs = require('fs');
-
-router.get('/', (req,res) => {
-  res.send('<h1>Hello World, Welcome to a Electron app!</h1>');
-});
-
-app.use('/', router);
-
-app.listen(process.env.port || 3000); */
-
-//console.log('Web Server is listening at port '+ (process.env.port || 3000));
-
-
 const express = require('express');
-const multer = require('multer');
-const upload = multer({ dest:'public/'});
-const exphbs = require('express-handlebars');
-const path = require('path');
-
 const app = express();
+var bodyParser = require('body-parser')
+var path = require('path');
 
-app.use(express.static(__dirname +'/server'));
+app.use("/", express.static(path.join(__dirname, 'public/')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//app.set('views', path.join(__dirname, 'pages/dropzone'));
-//app.set('view engine','html');
-app.set('views', path.join(__dirname, '../server/views'));
-app.set('view engine','pug');
-
-app.get('/', (req, res) => {
-  return res.render('index', {layout: false});
+app.get('/', (req,res) => {
+  res.sendFile('login.html', { root: __dirname + '/public/pages/' });
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  console.log(req.file); 
-  return res.status(200).send(req.file);
+app.post('/', function(req, res) {
+    //res.end(JSON.stringify(req.body));
+    res.redirect('/home');
 });
 
-app.listen(3000, () => {
-  console.log('Express server listening on port 3000');
+app.get('/home', (req, res) => {
+  res.sendFile('index.html', { root: __dirname + '/public/pages/dropzone' });
 });
+
+app.listen(process.env.port || 3000);
+
+console.log('Web Server is listening at port '+ (process.env.port || 3000));
