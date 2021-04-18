1fb60d6e6a46959b053829cb59961cffec59715d
cgalvan9 <cgalvan9@ucmerced.edu>
2021-04-18 12:45:56 -0700

    Add: added upload function to the server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser')
 var path = require('path');
+const multer = require('multer');
+const upload = multer({ dest:'src/public/datasets'});
 
 app.use("/", express.static(path.join(__dirname, 'public/')));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,6 +21,12 @@ app.get('/upload', (req, res) => {
   res.sendFile('index.html', { root: __dirname + '/public/pages/dropzone' });
 });
 
+app.post('/upload', upload.single('file'), (req, res) => {
+  console.log("Original file name: ", req.file.originalname);
+  console.log("Generated name: ", req.file.filename); 
+  return res.status(200).send(req.file); 
+})
+
 app.listen(process.env.port || 3000);
 
-console.log('Web Server is listening at port '+ (process.env.port || 3000));
+//console.log('Web Server is listening at port '+ (process.env.port || 3000));
