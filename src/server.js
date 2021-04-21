9d828d58c5d73c5a1c48382ae80c693b5c6c5d49
cgalvan9 <cgalvan9@ucmerced.edu>
2021-04-21 12:24:00 -0700

    change: Copied upload.hbs into index.html

--- src/server.js
+++ src/server.js
@@ -26,18 +26,19 @@ app.post('/upload', upload.single('file'), (req, res) => {
   //console.log("Generated name: ", req.file.filename); 
   const parse = require('./public/javascript/testDataOrg'); 
   parse.uploadParse(req.file.filename); 
-  res.redirect('/import'); 
+  //res.redirect('/import'); 
   return res.status(200).send(req.file); 
 })
 
-/* Not working yet 
+
+/*
 app.get('/import', (req, res) => {
   res.sendFile('import.html', { root: __dirname + '/public/pages' });
 });
 
 app.post('/import', (req, res) => {
-  const importt = require('./public/pages/visualViewer'); 
-  importt.readFile(req.file.filename); 
+  //const importt = require('./public/pages/visualViewer'); 
+  //importt.readFile(req.file.filename); 
 }); */ 
 
 
