37a93db08603b70cf50fff07c73f952a660183b7
Ernesto <56931380+Ernest0G@users.noreply.github.com>
2021-04-23 17:00:31 -0700

    add : added checks for valid column fields

--- src/public/javascript/visualViewer.js
+++ src/public/javascript/visualViewer.js
@@ -1,7 +1,9 @@
+// const { dataOrg } = require("./dataOrganizer");
+
 function readFile()
 {
   $.ajax({
-    url: 'src/public/datasets/cereal.csv',
+    url: '../../public/cereal.csv',
     dataType: 'text',
   }).done(showInitialFormat,showSweepFormat);
 }
@@ -63,7 +65,7 @@ function showSweepFormat(data)
 
 
   // will add header with attribute names for the user to know what they aer changing
-  var table = '<table>';  
+  var table = '<table id="SelectionTable">';  
 
   table += '<thead>';
   table += '<tr>';
@@ -76,8 +78,8 @@ function showSweepFormat(data)
           table += rowCells[rowCell];
           table += '</th>';
   }
-  table += '</tr>';
 
+  table += '</tr>';
 
        //Adding a new row that will contain the option to select the type each column will be (directory, timestamp, stream, ts_param, or not applicable)
        table += '<tr>';   
@@ -103,10 +105,115 @@ function showSweepFormat(data)
        table += '</table>';
        $('.columnMiddle').append(table);
 
+
+       var radioBtn = "<br><p>Have you selected one of your headers as a stream? </p><br><form><input type=\"radio\" name=\"choice\" value=\"yes\"> Yes ";
+       radioBtn = radioBtn + "<input type=\"radio\" name=\"choice\" value=\"no\"> No ";
+       radioBtn = radioBtn + "<input type=\"button\" id=\"btn\" value=\"Confirm\" onclick=getValueForStream()></form>";
+
+      //  radioBtn = radioBtn + "<script> const btn.onclick = function () {";
+      //  radioBtn = radioBtn + " const rbs = document.querySelectorAll('input[name=\"choice\"]');";
+      //  radioBtn = radioBtn + " let selectedValue;";
+      //  radioBtn = radioBtn + " for (const rb of rbs) { ";
+      //  radioBtn = radioBtn + "     if (rb.checked) { ";
+      //  radioBtn = radioBtn + "         selectedValue = rb.value; ";
+      //  radioBtn = radioBtn + "         break; ";
+      //  radioBtn = radioBtn + "     } ";
+      //  radioBtn = radioBtn + " } ";
+      //  radioBtn = radioBtn + " alert(selectedValue); ";
+      //  radioBtn = radioBtn + "}; ";
+      //  radioBtn = radioBtn + "</script>";
+
+       $('.columnMiddle').append(radioBtn);
+
+} 
+
+
+
+function getValueForStream(){
+  //get the value of the radio button to determine if the user has used a header as a stream
+  const btn = document.querySelector('#btn');
+
+  const rbs = document.querySelectorAll('input[name=\"choice\"]');
+  let selectedValue;
+  for(const rb of rbs){
+    if(rb.checked){
+      selectedValue = rb.value;
+      break;
+    }
+  }
+
+      //create a text box for user to add the name for the stream
+  if(selectedValue == "no"){
+    var textBox = "<br><p>Type the name you would like to call your stream.</p>";
+    textBox = textBox + "<input id =\"streamText\" placeholder=\"stream name\">";
+    $(".columnMiddle").append(textBox);
+  }
+
+
 }
 
-function editFormat()
-{
+function passToDataOrg()
+{ 
+  var table = document.getElementById("SelectionTable");
+  var columnFields = {};
 
+  // will be keeping count of these 3 values to make sure that the user does not input more than 1, and if streams remains as 0 then we ask the user if we can put all the data into one stream.
+  var dirCount = 0;
+  var streamCount = 0;
+  var timestampCount = 0;
+  var streamName = "";
+
+  //iterate through the column field selection table
+  for (var r = 0; r < 1; r++) {
+      for (var c = 0; c < table.rows[r].cells.length; c++) {
+          var key = table.rows[r].cells[c].innerHTML;
+          var adjustedcurrentColumnName= "#" +  key + " option:selected";
+          var value = $(adjustedcurrentColumnName).text();
+
+          //Checking which types are selected
+          if (value == "directory"){
+            dirCount++;
+          }
+          if(value == "stream"){
+            streamCount++;
+
+          }
+          if(value == "timestamp"){
+            timestampCount++;
+          }
+
+          //insert the key and value to the dictionary "columnFields", if value is a stream 
+          if(value == "stream"){
+            streamName = key;
+          }else{
+            columnFields[key]=value; 
+          }
+          
+      }
+  }
+
+  // current way to display if changes need to be made by the user as to what has been selected
+  if(dirCount > 1){
+    alert("You have chosen more than 1 directory, choose just one and submit again.");
+    return;
+  }else if(streamCount > 1){
+    alert("You have chosen more than 1 stream, choose just one and submit again.");
+    return;
+  }else if (timestampCount > 1){
+    alert("You have chosen more than 1 timestamp, choose just one and submit again.");
+    return;
+  }else if(dirCount == 0 || timestampCount == 0){
+    alert("You must have at least one directory and one timestamp.");
+    return;
+  }else if(streamCount == 0){
+    //get the name of the new stream from the id="streamText"
+    streamName = document.getElementById("streamText").value;
+    ;
+  }
+  
+
+  console.log(columnFields);
+  console.log(streamName);
+
+  dataOrg(columnFields, streamName);
 }
-
