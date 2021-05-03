48a39bb3770c2416669af8f43d748b1562e83aa9
Annisa Fahira <annisafahira999@gmail.com>
2021-05-03 02:32:42 -0700

    add: axios and implemented store library

--- src/login.js
+++ src/login.js
@@ -1,100 +1,103 @@
-var mysql = require('mysql2');
 var express = require('express');
-var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 const multer = require('multer');
 const upload = multer({ dest:'src/public/datasets'});
 
-var connection = mysql.createConnection({
-	//host     : 'localhost',
-	host  : "127.0.0.1",
-	port     : "3306",
-	user     : "root",
-	password : "project123",
-	database : "login",
-	socketPath  : "/tmp/mysql.sock",
-});
+const store = require("store2");
+const axios = require('axios');
 
 var app = express();
 
 app.use("/", express.static(path.join(__dirname, 'public/')));
 
-app.use(session({
-	secret: "sweepStoreAdmin",
-	resave: true,
-	saveUninitialized: true
-}));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 
 app.get('/', function(request, response) {
-	//response.sendFile(path.join(__dirname + '/login.html'));
     response.sendFile('login.html', { root: __dirname + '/public/pages/' });
 });
+/*
+const asyncLocalStorage = {
+    setItem: async function (id,key) {
+        await null;
+        return als.set(key);
+    },
+	setItem: async function (id,token) {
+        await null;
+        return als.set(token);
+    },
+    getItem: async function (key) {
+        await null;
+        return als.get(key);
+    },
+	getItem: async function (token) {
+        await null;
+        return als.get(token);
+    }
+};
+*/
 
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				request.session.username = password;
-				response.redirect('/upload');
+
+app.post('/', function(request, response) {
+	var userkey = request.body.userkey;
+	var usertoken = request.body.usertoken;
+
+	var getKey = store.get('key');
+	var getToken = store.get('token');
+
+	if(userkey && usertoken){
+		console.log("Check 1");
+		if (getKey == userkey && getToken == usertoken){
+			console.log("Check 2");
+		//check if key and token has already been saved
+			if (store.get('key') && store.get('token')!== null){
+					response.redirect('/upload');
+					console.log(response);
+			//check if key and token is valid
 			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
+				//still doesn't work
+				axios({
+							method: "post",
+							url: 'https://api.sweepapi.com/account/auth',
+							headers: {
+								'Content-Type' : 'application/json',
+							},
+							data: JSON.stringify({
+								'email': email,
+								'password': password,
+								}),
+				}).then(function (response) {
+							store.set('key', userkey);
+							store.set('token', usertoken);
+							response.redirect('/upload');
+							console.log(response);
+						});
+					}
+				}
 	} else {
-		response.send('Your Username and Password cannot be found!');
-		response.end();
-	}
+		console.log('User is not authenticated');
+		response.redirect('/login');
+		
+	} 
 });
 
 app.get('/upload', function(request, response) {
-	//if (request.session.loggedin) {
-    	response.sendFile('upload.html', { root: __dirname + '/public/pages' });
-		//response.send('Welcome back, ' + request.session.username + '!');
-	//} else {
-		//response.send('Please login to view this page!');
-	//}
-	//response.end();
+	response.sendFile('upload.html', { root: __dirname + '/public/pages' });
+
 });
+
 app.post('/upload', upload.single('file'), function(request, response) { 
 	const parse = require('./public/javascript/testDataOrg'); 
 	parse.uploadParse(request.file.filename);
 	return response.status(200).send(request.file);
-  })
-/*app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
-});*/
+	})
 
-//app.listen(3000);
-
-connection.connect((err) => {
-	if(err){
-	  console.log('Error connecting to DB');
-	  return;
-	}
-	console.log('Connection established to DB');
-  });
-
-  /*connection.end((err) => {
-	// The connection is terminated gracefully
-	// Ensures all remaining queries are executed
-	// Then sends a quit packet to the MySQL server.
-  });*/
+app.get('/login', function(request, response) {
+	response.sendFile('login.html', { root: __dirname + '/public/pages' });
+});
 
 app.listen(3000, () => {
     console.log('Express server listening on port 3000');
   });
-  
